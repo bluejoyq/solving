2cf08c3ac45f84229052521a637c89c6115b1cc4
bluejoyq <bluejoyq@naver.com>
2021-09-29 17:11:31 +0900

    fix check

--- bakjoon/18870.cpp
+++ bakjoon/18870.cpp
@@ -1,17 +1,26 @@
-// for checkg support C++ (using g++)
-//
-// g++ -std=c++98 main.cpp
-// g++ -std=c++03 main.cpp
-// g++ -std=c++0x main.cpp
-// g++ -std=c++11 main.cpp
-// g++ -std=c++14 main.cpp
-// g++ -std=c++17 main.cpp
 #include <iostream>
+#include <utility>
+#include <algorithm>
 using namespace std;
-int main(){
-int cpp = (int) __cplusplus;
-int year = cpp / 100; // 1997 ... 2017
-int month = cpp % 100;
-cout << "Year:" << year << " Month:" << month << endl;
-return 0;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+
+    int N;
+    cin >> N;
+    pair<int,int> values[N];
+    int results[N];
+
+    for(int i = 0; i < N; i++){
+        cin >> values[i].first;
+        values[i].second = values[i].first;
+    }
+
+    sort(values, values + N);
+
+
+    return 0;
 }
