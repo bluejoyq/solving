88f7b9edc0257a459e0250b66e339383ec62ff14
BuildTools <unconfigured@null.spigotmc.org>
2021-09-29 17:07:54 +0900

    delete .vscode

--- bakjoon/10818.cpp
+++ bakjoon/10818.cpp
@@ -2,19 +2,20 @@
 
 using namespace std;
 int main(){
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    int N, tmp;
-    std::cin >> N;
-    int min_value = 876543210;
-    int max_value = -876543210;
-    for(int i = 0; i < N; i++){
-        std::cin >> tmp;
-        min_value = std::min(tmp, min_value);
-        max_value = std::max(tmp, max_value);
-    }
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int N, tmp;
+	std::cin >> N;
+	int min_value = 876543210;
+	int max_value = -876543210;
+	for(int i = 0; i < N; i++){
+		std::cin >> tmp;
+		min_value = std::min(tmp, min_value);
+		max_value = std::max(tmp, max_value);
+	}
+	
 
-    std::cout << min_value << ' ' << max_value;
-    return 0;
+	std::cout << min_value << ' ' << max_value;
+	return 0;
 }
--- bakjoon/2231.cpp
+++ bakjoon/2231.cpp
@@ -3,25 +3,25 @@
 using namespace std;
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    int N;
-    cin >> N;
-    for(int i = 0; i < N; i++){
-        int tmp = i;
-        for(int j = 1; j <= i; j *= 10){
-            tmp += (i % (j * 10)) / j;
-        }
-        //cout << tmp << endl;
-        //cout << i << ' ' << tmp << endl;
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int N;
+	cin >> N;
+	for(int i = 0; i < N; i++){
+		int tmp = i;
+		for(int j = 1; j <= i; j *= 10){
+			tmp += (i % (j * 10)) / j;
+		}
+		//cout << tmp << endl;
+		//cout << i << ' ' << tmp << endl;
 
-        if (tmp == N) {
-            cout << i;
-            return 0;
-        }
-        
-    }
-    cout << 0;
-    return 0;
+		if (tmp == N) {
+			cout << i;
+			return 0;
+		}
+		
+	}
+	cout << 0;
+	return 0;
 }
