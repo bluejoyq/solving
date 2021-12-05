6bbacbcb3c4c23578930d554c8eec9b5e867eaae
bluejoyq <bluejoyq@naver.com>
2021-12-05 18:57:33 +0900

    맞은 문제들을 o 폴더로 이동

---
+++ bakjoon/o/2231.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int N;
+	cin >> N;
+	for(int i = 0; i < N; i++){
+		int tmp = i;
+		for(int j = 1; j <= i; j *= 10){
+			tmp += (i % (j * 10)) / j;
+		}
+		//cout << tmp << endl;
+		//cout << i << ' ' << tmp << endl;
+
+		if (tmp == N) {
+			cout << i;
+			return 0;
+		}
+		
+	}
+	cout << 0;
+	return 0;
+}
