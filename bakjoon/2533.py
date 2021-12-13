497fa4d3b40e60cb33e4c71c0b47841c81ae2c1b
bluejoyq <bluejoyq@naver.com>
2021-12-13 21:19:33 +0900

    2533 time over

--- bakjoon/2533.py
+++ bakjoon/2533.py
@@ -1,42 +1,37 @@
 # 각 level의 수를 비교해야함.
 import sys
+from collections import deque
 input =sys.stdin.readline
+sys.setrecursionlimit(1000000)
 N = int(input())
 
 edges = [{} for i in range(N)]
 for i in range(N-1):
 	u,v = map(int, input().split())
 	edges[u-1][v-1] = 1
-	edges[u-1][v-1] = 1
+	edges[v-1][u-1] = 1
 
-# 각 레벨의 노드 수 구하기
 visited = [0] * N
-node_count_by_level = [0] * N
-nxt_findings = [0]
-cur_level = 0
-while nxt_findings:
-	findings = nxt_findings
-	nxt_findings = []
-	node_count_by_level[cur_level] = len(findings)
-	cur_level += 1
+cache = [N] * N
+def dfs(cur, is_essential):
+	visited[cur] = 1
+	selected_result = 1
+	not_selected_result = 0
+
+	for nxt in edges[cur].keys():
+		if visited[nxt]:
+			continue
+		selected_result += dfs(nxt, 0)
+	if is_essential:
+		visited[cur] = 0
+		return selected_result
+
+	for nxt in edges[cur].keys():
+		if visited[nxt]:
+			continue
+		not_selected_result += dfs(nxt, 1)
+	visited[cur] = 0
+	return min(selected_result,not_selected_result)
 	
-	while findings:
-		cur = findings.pop()
-		visited[cur] = 1
-		for nxt in edges[cur].keys():
-			if visited[nxt]:
-				continue
-			nxt_findings.append(nxt)
 
-# dp
-cache= [[N,N] for i in range(N+2)]
-cache[0][0] = 0
-for i in range(N):
-	if node_count_by_level[i] == 0:
-		print(min(cache[i]), cache)
-		print(node_count_by_level)
-		break
-	cache[i+1][1] = min(cache[i + 1][1],cache[i][0])
-	cache[i+1][0] = min(cache[i + 1][0],cache[i][1] + node_count_by_level[i])
-
-
+print(dfs(0, 0))
