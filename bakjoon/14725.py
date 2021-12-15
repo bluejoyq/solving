cb2dcf993fc4bac2514be058a9f472d3c1c926d9
bluejoyq <bluejoyq@naver.com>
2021-12-15 16:53:53 +0900

    14725 dfs recursive

---
+++ bakjoon/14725.py
@@ -0,0 +1,22 @@
+import sys
+input = sys.stdin.readline
+print = sys.stdout.write
+N = int(input())
+root = {}
+for i in range(N):
+	values = list(input().split())
+	cur = root
+	for value in values[1:]:
+		try:
+			cur = cur[value]
+		except:
+			cur[value] = {}
+			cur = cur[value]
+
+
+cur = root
+def dfs(cur, depth):
+	for nxt in sorted(cur.keys()):
+		print('--' * depth + nxt + '\n')
+		dfs(cur[nxt], depth + 1)
+dfs(root, 0)
--- bakjoon/2213.py
+++ bakjoon/2213.py
@@ -12,47 +12,33 @@ for i in range(N-1):
 	edges[b - 1][a - 1] = 1
 
 cache = [[0,0] for i in range(N)]
-
 visited = [0] * N
 findings = [0]
+visited_nodes = []
 
-cache[0][1] += nodes[0]
 while findings:
 	cur = findings.pop()
 	visited[cur] = 1
+	visited_nodes.append(cur)
 	cache[cur][1] += nodes[cur]
 	for nxt in edges[cur].keys():
 		if visited[nxt]:
 			continue
-		cache[nxt][0] = cache[cur][1] + nodes[nxt]
-		cache[nxt][1] = cache[cur][0]
+		cache[nxt][0] = cache[cur][1]
+		cache[nxt][1] = min(cache[cur][1], cache[cur][0])
 		findings.append(nxt)
 
-max_val = -1
-max_idx = [-1,-1]
-for i in range(N):
-	if max_val < cache[i][0]:
-		max_val = cache[i][0]
-		max_idx = [i,0]
-	if max_val < cache[i][1]:
-		max_val = cache[i][1]
-		max_idx = [i,1]
-print(max_val)
-print(*max_idx)
-result = []
-cur = max_idx[0]
-cur_sub = max_idx[1]
-while max_val > 0:
-	max_val -= nodes[cur]
-	result.append(cur + 1)
-	visited[cur] = 2
 
-	for nxt in edges[cur].keys():
-		if visited[nxt] == 2:
-			continue
-		if max_val == cache[nxt][1- cur_sub]:
-			cur = nxt
-			cur_sub = 1-cur_sub
-			break
+max_val = max(cache[N - 1])
+max_idx = N-1
+result = []
+
+
+print(visited_nodes, cache)
+for i in range(1, N):
+	bef = visited_nodes[i-1]
+	cur = visited_nodes[i]
+
+	print(cache[cur])
 result.sort()
 print(result)
