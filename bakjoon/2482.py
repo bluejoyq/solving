b8c7ec11e21d1ed42f26d820ebc1675ba893bce1
bluejoyq <bluejoyq@naver.com>
2022-01-17 01:30:17 +0900

    2482 ver 1 solved

--- bakjoon/2482.py
+++ bakjoon/2482.py
@@ -1,19 +1,27 @@
 N = int(input())
 K = int(input())
 
-
+LARGE_NUM = 1000000003
 # 0번째 고르는 경우 
-cache = [[0] * (K + 1) for i in range(N)]
+cache = [[0] * (K + 1) for i in range(N + 2)]
 cache[0][1] = 1
+result = 0
+for i in range(N - 1):
+    for j in range(K):
+        cache[i + 1][j] = (cache[i+1][j] + cache[i][j]) % LARGE_NUM
+        cache[i + 2][j+1] = (cache[i + 2][j+1] + cache[i][j]) % LARGE_NUM
+    result += cache[i][K]
+tmp = result
+
+# 0번째를 고르지 않는 경우
+cache = [[0] * (K + 1) for i in range(N + 2)]
+cache[1][0] = 1
+result = 0
 for i in range(1,N):
-    for j in range(K + 1):
-        cache[i][j] = cache[i - 1][j] + cache[i-2][j-1]
-zero_pick = cache[N][K]
-print(cache)
-cache = [[0] * (K + 1) for i in range(N)]
-cache[0][0] = 1
-for i in range(1,N):
-    for j in range(K + 1):
-        cache[i][j] = cache[i - 1][j] + cache[i-2][j-1]
-print(cache)
-print(zero_pick + cache[N-1][K])
+    for j in range(K):
+        cache[i + 1][j] = (cache[i+1][j] + cache[i][j]) % LARGE_NUM
+        cache[i + 2][j+1] = (cache[i + 2][j+1] + cache[i][j]) % LARGE_NUM
+    result += cache[i][K]
+for i in range(N, N+2):
+    result += cache[i][K]
+print((tmp + result) % LARGE_NUM)
