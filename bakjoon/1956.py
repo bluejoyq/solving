84a6cb1d5d0b6140224ea55b381f868733228773
bluejoyq <bluejoyq@naver.com>
2022-01-17 23:14:52 +0900

    1956 solve but sooo slooooooooooooow

--- bakjoon/1956.py
+++ bakjoon/1956.py
@@ -4,65 +4,41 @@
 from heapq import heappush, heappop 
 import sys
 input= sys.stdin.readline
+
+
+
 def solve():
     V,E = map(int, input().split())
-    parents = [i for i in range(V + 1)]
-    ranks = [0] * (V+1)
-
-    def find(x):
-        # 경로 압축 ㄴㄴ
-        if x == parents[x]:
-            return x
-        return find(parents[x])
-
-    def union(x,y):
-        x = find(x)
-        y = find(y)
-
-        if x == y:
-            return 0
-        
-        if ranks[x] > ranks[y]:
-            parents[y] = x
-            ranks[x] += 1
-        else:
-            parents[x] = y
-            ranks[y] += 1
-        return 1
-
-    edges = []
+    edges = [{} for i in range(V+1)]
     for i in range(E):
         a,b,c = map(int, input().split())
-        heappush(edges,(c,a,b))
-
-    edges_dict = [{} for i in range(V+1)]
-    passed = 0
-    # 일단 출발지와 도착지를 찾자.
-    while edges:
-        cost,a,b = heappop(edges)
-        edges_dict[a][b] = cost
-        if union(a,b):
-            continue
-        passed = 1
-        break
-    if not passed:
-        return -1
-    # 그리고 a에서 b로 가는 최단 루트 찾기
-    visited = [0] *(V+1)
-    print(a,b)
-    findings = []
-    for nxt in edges_dict[a].keys():
-        if visited[nxt]:
-            continue
-        heappush(findings,(0 + edges_dict[a][nxt], nxt))
-    while findings:
-        cost,cur = heappop(findings)
-        visited[cur] = 1
-        if cur == a:
-            return cost
-
-        for nxt in edges_dict[cur].keys():
+        edges[a][b] = c
+    def find_fastest_cycle(start):
+        visited = [0] *(V+1)
+        findings = []
+        for nxt in edges[start].keys():
             if visited[nxt]:
                 continue
-            heappush(findings,(cost + edges_dict[cur][nxt], nxt))
+            heappush(findings,(0 + edges[start][nxt], nxt))
+        while findings:
+            cost,cur = heappop(findings)
+            visited[cur] = 1
+            if cur == start:
+                return cost
+
+            for nxt in edges[cur].keys():
+                if visited[nxt]:
+                    continue
+                heappush(findings,(cost + edges[cur][nxt], nxt))
+        return -1
+    result = 9876543210
+    for i in range(1, V+1):
+        cost = find_fastest_cycle(i)
+        if cost == -1:
+            continue
+        result = min(cost,result)
+    if result == 9876543210:
+        result = -1
+    return result
+    
 print(solve())
