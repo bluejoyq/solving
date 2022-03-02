d5b1d42670f44c2a79b2b9c760a4b34cd4280a14
bluejoyq <bluejoyq@naver.com>
2022-03-02 15:04:44 +0900

    재귀와 dp를 사용하게 업데이트

--- bakjoon/17435.py
+++ bakjoon/17435.py
@@ -1,12 +1,24 @@
 import sys
+sys.setrecursionlimit(5000001)
 input = sys.stdin.readline
 
 M = int(input())
-edges = [{} for i in range(M+1)]
-for i in range(1, M + 1):
-    edges[i][int(input())] = 1
 
+values = [0] + list(map(int, input().split()))
+cache = [{} for i in range(M+1)]
+result = []
 Q = int(input())
+
+def find_nested_result(n,x):
+    if n == 0:
+        return x
+    try:
+        return cache[x][n]
+    except:
+        pass
+    cache[x][n] = find_nested_result(n - 1, values[x])
+    return cache[x][n]
 for i in range(Q):
     n,x = map(int, input().split())
-    
+    result.append(str(find_nested_result(n,x)))
+print('\n'.join(result))
